6e43311e5729083c6a338d5f63eebad3bdd5cae0
Fiasko99 <50075765+Fiasko99@users.noreply.github.com>
2020-05-07 14:30:48 +0300

    finishVariant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//глобально
+//import MyPerson from './components/myperson.vue'
+
 Vue.config.productionTip = false
+
+//глобально
+//Vue.component('app-person', MyPerson)
 
 new Vue({
   render: h => h(App),
